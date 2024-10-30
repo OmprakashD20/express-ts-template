410c3eea2c19578f6744c8c84693478ae52cecad
Omprakash  D <omugan07@gmail.com>
2024-10-31 01:45:32 +0530

    chore: type inferences for drizzle schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,32 @@
 import { TObject } from "@sinclair/typebox";
+import type {
+  BuildQueryResult,
+  DBQueryConfig,
+  ExtractTablesWithRelations,
+} from "drizzle-orm";
+
+import * as schema from "@/drizzle/schema";
+
+type Schema = typeof schema;
+type TSchema = ExtractTablesWithRelations<Schema>;
+
+type QueryConfig<TableName extends keyof TSchema> = DBQueryConfig<
+  "one" | "many",
+  boolean,
+  TSchema,
+  TSchema[TableName]
+>;
+
+export type InferResultType<
+  TableName extends keyof TSchema,
+  QBConfig extends QueryConfig<TableName> = {}
+> = BuildQueryResult<TSchema, TSchema[TableName], QBConfig>;
+
+export function enumToPgEnum<T extends Record<string, any>>(
+  myEnum: T
+): [T[keyof T], ...T[keyof T][]] {
+  return Object.values(myEnum).map((value: any) => `${value}`) as any;
+}
 
 export type Prettify<T> = {
   [K in keyof T]: T[K];
